ec6c2ddd3ff227ed8aefaa23a2363af508b8ed76
QQBoxy <gs1458@gmail.com>
2023-02-14 17:43:05 +0800

    Add Exception handling

--- commands/openai.js
+++ commands/openai.js
@@ -13,24 +13,28 @@ module.exports = {
             .setDescription('Input question')
         ),
     async execute(interaction) {
-        await interaction.deferReply();
-        const question = interaction.options.getString('question');
-        const response = await axios({
-            method: 'post',
-            url: "https://api.openai.com/v1/completions",
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.OPENAI_TOKEN}`
-            },
-            data: {
-                model: "text-davinci-003",
-                prompt: question,
-                temperature: 0.9,
-                max_tokens: 2000
-            }
-        });
-        const answer = Lodash.get(response, 'data.choices[0].text', '').substring(2);
-        const result = `Q:${question}\nA:${answer}`;
-        return interaction.editReply(result);
+        try {
+            await interaction.deferReply();
+            const question = interaction.options.getString('question');
+            const response = await axios({
+                method: 'post',
+                url: "https://api.openai.com/v1/completions",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${process.env.OPENAI_TOKEN}`
+                },
+                data: {
+                    model: "text-davinci-003",
+                    prompt: question,
+                    temperature: 0.9,
+                    max_tokens: 2000
+                }
+            });
+            const answer = Lodash.get(response, 'data.choices[0].text', '').substring(2);
+            const result = `Q:${question}\nA:${answer}`;
+            return interaction.editReply(result);
+        } catch (e) {
+            return interaction.editReply("BotBoxy: Command failed :(");
+        }
     },
 };
